88b5cc8fad76ac2050954fd57cc30bd241da5a30
Gregory John Casamento <greg.casamento@gmail.com>
2019-07-07 05:09:39 -0400

    Add all files

---
+++ NSUbiquitousKeyValueStore_test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSUbiquitousKeyValueStore_test
+//
+//  Created by Gregory John Casamento on 7/7/19.
+//  Copyright © 2019 Gregory John Casamento. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
